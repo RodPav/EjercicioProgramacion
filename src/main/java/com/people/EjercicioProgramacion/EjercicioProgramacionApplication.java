bc69c4e4c2b4aa0a144255eaa52678afe4477cbf
Rodrigo <rpavezvalenzuela@gmail.com>
2020-03-21 10:28:28 -0300

    first commit

---
+++ src/main/java/com/people/EjercicioProgramacion/EjercicioProgramacionApplication.java
@@ -0,0 +1,13 @@
+package com.people.EjercicioProgramacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioProgramacionApplication.class, args);
+	}
+
+}
