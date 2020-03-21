7c7b68a163e27c9ec14983b2e0931d35ee476d0d
Rodrigo <rpavezvalenzuela@gmail.com>
2020-03-21 13:39:08 -0300

    feat: ComponentScan, EnableJpaRepositories and EntityScan Annotations

--- src/main/java/com/people/EjercicioProgramacion/EjercicioProgramacionApplication.java
+++ src/main/java/com/people/EjercicioProgramacion/EjercicioProgramacionApplication.java
@@ -2,8 +2,14 @@ package com.people.EjercicioProgramacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.people.EjercicioProgramacion")
+@EnableJpaRepositories(basePackages = "repositories")
+@EntityScan("domain")
 public class EjercicioProgramacionApplication {
 
 	public static void main(String[] args) {
